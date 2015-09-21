ebb75cf363b20566aa7754ce7df603bdf8336c75
Dinko Ivanov <dinko.ivanov@gmail.com>
2015-09-21 19:59:02 +0300

    example

--- limpet-prototype/src/limpet/prototype/App.java
+++ limpet-prototype/src/limpet/prototype/App.java
@@ -1,14 +1,15 @@
 package limpet.prototype;
 
-import javax.measure.Unit;
-import javax.measure.quantity.Speed;
+import javax.measure.Quantity;
+import javax.measure.quantity.Mass;
 
-import tec.units.ri.unit.MetricPrefix;
+import systems.uom.common.US;
+import tec.units.ri.quantity.Quantities;
 import tec.units.ri.unit.Units;
 
 public class App {
 	public static void main(String[] args) {
-		Unit<Speed> kmh = MetricPrefix.KILO(Units.METRE).divide(Units.HOUR).asType(Speed.class);
-		System.out.println(kmh.toString());
+		Quantity<Mass> mass = Quantities.getQuantity(70, Units.KILOGRAM);
+		System.out.println(mass.to(US.POUND).getValue());
 	}
 }
