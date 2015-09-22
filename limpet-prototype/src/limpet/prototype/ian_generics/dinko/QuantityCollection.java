cf2c8eed429356fd1dfeb59613a333a9e83dd27d
Ian Mayo <ian@planetmayo.com>
2015-09-22 13:22:44 +0100

    minor tidying

--- limpet-prototype/src/limpet/prototype/ian_generics/dinko/QuantityCollection.java
+++ limpet-prototype/src/limpet/prototype/ian_generics/dinko/QuantityCollection.java
@@ -2,7 +2,7 @@ package limpet.prototype.ian_generics.dinko;
 
 import javax.measure.Quantity;
 
-public class QuantityCollection<T extends Quantity<T>> extends ObjectCollection<T>
+public class QuantityCollection<T extends Quantity<T>> extends ObjectCollection<Quantity<T>>
 {
 
 }
