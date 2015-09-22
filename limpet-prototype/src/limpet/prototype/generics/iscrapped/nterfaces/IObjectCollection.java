f8520de9729ba00247bf0a656bdd5d078a9a2f3e
Ian Mayo <ian@planetmayo.com>
2015-09-22 16:31:54 +0100

    ditch the previous attempt

--- limpet-prototype/src/limpet/prototype/generics/iscrapped/nterfaces/IObjectCollection.java
+++
@@ -1,9 +0,0 @@
-package limpet.prototype.generics.iscrapped.nterfaces;
-
-import java.util.Collection;
-
-public interface IObjectCollection<T extends Object> {
-	public void add(T object);
-	
-	public Collection<T> getValues();
-}
--- limpet-prototype/src/limpet/prototype/generics/iscrapped/nterfaces/IQuantityCollection.java
+++
@@ -1,9 +0,0 @@
-package limpet.prototype.generics.iscrapped.nterfaces;
-
-import javax.measure.Quantity;
-
-public interface IQuantityCollection<T extends Quantity<T>> extends IBaseQuantityCollection<T> {
-
-	public void add(Quantity<T> quantity);
-
-}
