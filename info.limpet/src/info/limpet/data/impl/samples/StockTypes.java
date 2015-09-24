bb2a21f921b5bd8a6d8bc43660813aae02604989
Ian Mayo <ian@planetmayo.com>
2015-09-24 09:10:33 +0100

    introduce some new stock types

--- info.limpet/src/info/limpet/data/impl/samples/StockTypes.java
+++ info.limpet/src/info/limpet/data/impl/samples/StockTypes.java
@@ -2,20 +2,27 @@ package info.limpet.data.impl.samples;
 
 import info.limpet.data.impl.QuantityCollection;
 import info.limpet.data.impl.TemporalQuantityCollection;
+import info.limpet.data.impl.ObjectCollection;
+import info.limpet.data.impl.TemporalObjectCollection;
 
 import javax.measure.quantity.Acceleration;
+import javax.measure.quantity.Angle;
+import javax.measure.quantity.Dimensionless;
+import javax.measure.quantity.Frequency;
 import javax.measure.quantity.Length;
 import javax.measure.quantity.Speed;
+import javax.measure.quantity.Time;
 
 import tec.units.ri.unit.Units;
 
 public class StockTypes
 {
 
-	/** time series (temporal) collections
+	/**
+	 * time series (temporal) collections
 	 * 
 	 * @author ian
-	 *
+	 * 
 	 */
 	public static class Temporal
 	{
@@ -50,12 +57,67 @@ public class StockTypes
 						.asType(Acceleration.class));
 			}
 		}
+
+		public static class ElapsedTime_Sec extends
+				TemporalQuantityCollection<Time>
+		{
+			public ElapsedTime_Sec(String name)
+			{
+				super(name, Units.SECOND.asType(Time.class));
+			}
+		}
+
+		public static class Frequency_Hz extends
+				TemporalQuantityCollection<Frequency>
+		{
+			public Frequency_Hz(String name)
+			{
+				super(name, Units.HERTZ.asType(Frequency.class));
+			}
+		}
+
+		public static class AcousticStrength extends
+				TemporalQuantityCollection<Dimensionless>
+		{
+			public AcousticStrength(String name)
+			{
+				super(name, Units.ONE.asType(Dimensionless.class));
+			}
+		}
+
+		public static class Angle_Radians extends TemporalQuantityCollection<Angle>
+		{
+			public Angle_Radians(String name)
+			{
+				super(name, Units.RADIAN.asType(Angle.class));
+			}
+		}
+
+		public static class Angle_Degrees extends TemporalQuantityCollection<Angle>
+		{
+			public Angle_Degrees(String name)
+			{
+				// TODO: use units.mulitply - something like this:
+				// Units.RADIAN.multiply(2 * Math.PI);
+				super(name, Units.RADIAN.asType(Angle.class));
+			}
+		}
+
+		public static class Locations extends TemporalObjectCollection<TmpLocationItem>
+		{
+			public Locations(String name)
+			{
+				super(name);
+			}
+		}
+		
 	}
 
-	/** non-time series types
+	/**
+	 * non-time series types
 	 * 
 	 * @author ian
-	 *
+	 * 
 	 */
 	public static class NonTemporal
 	{
@@ -75,13 +137,35 @@ public class StockTypes
 			}
 		}
 
-		public static class Acceleration_MSecSec extends QuantityCollection<Acceleration>
+		public static class Acceleration_MSecSec extends
+				QuantityCollection<Acceleration>
 		{
 			public Acceleration_MSecSec(String name)
 			{
 				super(name, Units.METRE.divide(Units.SECOND).divide(Units.SECOND)
 						.asType(Acceleration.class));
 			}
+		}
+		
+
+		public static class Locations extends ObjectCollection<TmpLocationItem>
+		{
+			public Locations(String name)
+			{
+				super(name);
+			}
+		}
+
+	}
+
+	public static class TmpLocationItem
+	{
+		public double latVal, longVal, depthVal;
+		public TmpLocationItem(double latV, double longV, double depthV)
+		{
+			latVal = latV;
+			longVal = longV;
+			depthVal = depthV;
 		}
 	}
 
