0fc32749f08ff66588ba081ab4c9333482d17c96
Ian Mayo <ian@planetmayo.com>
2015-09-28 10:17:23 +0100

    introduce singleton object

--- info.limpet/src/info/limpet/data/impl/samples/SampleData.java
+++ info.limpet/src/info/limpet/data/impl/samples/SampleData.java
@@ -3,9 +3,11 @@ package info.limpet.data.impl.samples;
 import info.limpet.ICollection;
 import info.limpet.ICommand;
 import info.limpet.IObjectCollection;
+import info.limpet.IQuantityCollection;
 import info.limpet.IStore;
 import info.limpet.ITemporalQuantityCollection;
 import info.limpet.data.impl.ObjectCollection;
+import info.limpet.data.impl.QuantityCollection;
 import info.limpet.data.operations.AddQuantityOperation;
 import info.limpet.data.store.InMemoryStore;
 
@@ -14,7 +16,11 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
+import javax.measure.Unit;
+import javax.measure.quantity.Dimensionless;
 import javax.measure.quantity.Length;
+
+import tec.units.ri.unit.Units;
 
 public class SampleData
 {
@@ -38,6 +44,8 @@ public class SampleData
 				"String one");
 		IObjectCollection<String> string2 = new ObjectCollection<String>(
 				"String two");
+		IQuantityCollection<Dimensionless> singleton1 = new QuantityCollection<Dimensionless>(
+				"Floating point factor", Units.ONE.asType(Dimensionless.class));
 
 		long thisTime = 0; 
 		
@@ -57,6 +65,9 @@ public class SampleData
 		// add an extra item to speedSeries3
 		speedSeries3.add(thisTime + 12 * 500 * 60, 25);
 		
+		// give the singleton a value		
+		singleton1.add(4d);
+		
 		List<ICollection> list = new ArrayList<ICollection>();
 
 		list.add(speedSeries1);
@@ -66,6 +77,7 @@ public class SampleData
 		list.add(length2);
 		list.add(string1);
 		list.add(string2);
+		list.add(singleton1);
 
 		res.add(list);
 
