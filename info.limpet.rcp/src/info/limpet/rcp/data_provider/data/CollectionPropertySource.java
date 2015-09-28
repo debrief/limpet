0c0f864f3496a71030fea69ce52230ae43794757
Ian Mayo <ian@planetmayo.com>
2015-09-28 13:24:29 +0100

    add support for editing the quantity if a singleton is selected

--- info.limpet.rcp/src/info/limpet/rcp/data_provider/data/CollectionPropertySource.java
+++ info.limpet.rcp/src/info/limpet/rcp/data_provider/data/CollectionPropertySource.java
@@ -1,5 +1,11 @@
 package info.limpet.rcp.data_provider.data;
 
+import info.limpet.ICollection;
+import info.limpet.IQuantityCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
 import org.eclipse.ui.views.properties.IPropertySource;
 import org.eclipse.ui.views.properties.PropertyDescriptor;
@@ -14,6 +20,7 @@ public class CollectionPropertySource implements IPropertySource
 	private static final String PROPERTY_NAME = "limpet.collection.name";
 	private static final String PROPERTY_SIZE = "limpet.collection.size";
 	private static final String PROPERTY_DESCRIPTION = "limpet.collection.description";
+	private static final String PROPERTY_VALUE = "limpet.collection.value";
 
 	private IPropertyDescriptor[] propertyDescriptors;
 	private final CollectionWrapper _collection;
@@ -43,10 +50,11 @@ public class CollectionPropertySource implements IPropertySource
 	{
 		if (propertyDescriptors == null)
 		{
-			// Create a descriptor and set a category
+			List<PropertyDescriptor> dList = new ArrayList<PropertyDescriptor>();
+			
 			final PropertyDescriptor textDescriptor = new TextPropertyDescriptor(
 					PROPERTY_NAME, "Name");
-			textDescriptor.setCategory("Label");
+			textDescriptor.setCategory("Label");			
 			final PropertyDescriptor sizeDescriptor = new PropertyDescriptor(
 					PROPERTY_SIZE, "Size");
 			sizeDescriptor.setCategory("Metadata");
@@ -54,8 +62,20 @@ public class CollectionPropertySource implements IPropertySource
 					PROPERTY_DESCRIPTION, "Description");
 			descriptionDescriptor.setCategory("Label");
 
-			propertyDescriptors = new IPropertyDescriptor[]
-			{ textDescriptor, sizeDescriptor, descriptionDescriptor };
+			// see if we want to add a value editor
+			if(_collection.getCollection().size() == 1)
+			{
+				final PropertyDescriptor valueDescriptor = new TextPropertyDescriptor(
+						PROPERTY_VALUE, "Value");
+				valueDescriptor.setCategory("Value");
+				dList.add(valueDescriptor);
+			}
+			
+			dList.add(textDescriptor);
+			dList.add(sizeDescriptor);
+			dList.add(descriptionDescriptor);
+			
+			propertyDescriptors = dList.toArray(new IPropertyDescriptor[]{});
 		}
 		return propertyDescriptors;
 	}
@@ -71,6 +91,15 @@ public class CollectionPropertySource implements IPropertySource
 			return _collection.getCollection().size();
 		else if (prop.equals(PROPERTY_DESCRIPTION))
 			return _collection.getCollection().getDescription();
+		else if (prop.equals(PROPERTY_VALUE))
+		{
+			ICollection theColl = _collection.getCollection();
+			if(theColl instanceof IQuantityCollection<?>)
+			{
+				IQuantityCollection<?> tt = (IQuantityCollection<?>) theColl;
+				return "" + tt.getValues().iterator().next();
+			}
+		}
 
 		return null;
 	}
@@ -100,6 +129,15 @@ public class CollectionPropertySource implements IPropertySource
 			throw new RuntimeException("Can't set size, silly");
 		else if (prop.equals(PROPERTY_DESCRIPTION))
 			_collection.getCollection().setDescription((String) value);
+		else if (prop.equals(PROPERTY_VALUE))
+		{
+			ICollection theColl = _collection.getCollection();
+			if(theColl instanceof IQuantityCollection<?>)
+			{
+				IQuantityCollection<?> tt = (IQuantityCollection<?>) theColl;
+				tt.replaceSingleton(Double.parseDouble((String) value));
+			}
+		}
 	}
 
 }
