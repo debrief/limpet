db7d9d9773a4d0523a3c5d0123e033be43c1d4e0
Ian Mayo <ian@planetmayo.com>
2015-09-29 14:21:53 +0100

    define running total as a double - so we get double calculations

--- info.limpet/src/info/limpet/data/operations/MultiplyQuantityOperation.java
+++ info.limpet/src/info/limpet/data/operations/MultiplyQuantityOperation.java
@@ -173,7 +173,7 @@ public class MultiplyQuantityOperation implements
 			// start adding values.
 			for (int j = 0; j < length; j++)
 			{
-				Quantity<?> runningTotal = Quantities.getQuantity(1, unit);
+				Quantity<?> runningTotal = Quantities.getQuantity(1d, unit);
 
 				for (int i = 0; i < _inputs.size(); i++)
 				{
