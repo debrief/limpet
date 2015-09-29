8855128ad187bc57a406b21004455e61746112ce
Ian Mayo <ian@planetmayo.com>
2015-09-29 15:16:36 +0100

    use a copy constructor, since the collection we use may later change

--- info.limpet/src/info/limpet/data/commands/AbstractCommand.java
+++ info.limpet/src/info/limpet/data/commands/AbstractCommand.java
@@ -6,39 +6,40 @@ import info.limpet.IStore;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
-public abstract class AbstractCommand<T extends ICollection> implements ICommand<T>
+public abstract class AbstractCommand<T extends ICollection> implements
+		ICommand<T>
 {
 	final private String _title;
 	final private String _description;
 	final private boolean _canUndo;
 	final private boolean _canRedo;
 	final private IStore _store;
-	
-	final protected List<T> _inputs;		
+
+	final protected List<T> _inputs;
 	final protected List<T> _outputs;
 
-	public AbstractCommand(String title, String description, IStore store, boolean canUndo, boolean canRedo, List<T> inputs)
+	public AbstractCommand(String title, String description, IStore store,
+			boolean canUndo, boolean canRedo, List<T> inputs)
 	{
 		_title = title;
 		_description = description;
 		_store = store;
-		_canUndo = canUndo;		
+		_canUndo = canUndo;
 		_canRedo = canRedo;
-		
-		_inputs = inputs;
+
+		_inputs = new ArrayList<T>(inputs);
 		_outputs = new ArrayList<T>();
 	}
-	
-	
-	
+
 	@Override
 	public void dataChanged()
 	{
 		// do the recalc
 		recalculate();
-		
+
 		// now tell the outputs they have changed
 		Iterator<T> iter = _outputs.iterator();
 		while (iter.hasNext())
@@ -50,19 +51,16 @@ public abstract class AbstractCommand<T extends ICollection> implements ICommand
 
 	abstract protected void recalculate();
 
-
 	@Override
 	public void collectionDeleted()
 	{
 	}
 
-
-
 	public IStore getStore()
 	{
 		return _store;
 	}
-	
+
 	@Override
 	public String getTitle()
 	{
@@ -83,20 +81,22 @@ public abstract class AbstractCommand<T extends ICollection> implements ICommand
 		while (iter.hasNext())
 		{
 			T t = (T) iter.next();
-			t.addChangeListener(this);			
+			t.addChangeListener(this);
 		}
 	}
 
 	@Override
 	public void undo()
 	{
-		throw new UnsupportedOperationException("Should not be called, undo not provided");
+		throw new UnsupportedOperationException(
+				"Should not be called, undo not provided");
 	}
 
 	@Override
 	public void redo()
 	{
-		throw new UnsupportedOperationException("Should not be called, redo not provided");
+		throw new UnsupportedOperationException(
+				"Should not be called, redo not provided");
 	}
 
 	@Override
@@ -115,18 +115,16 @@ public abstract class AbstractCommand<T extends ICollection> implements ICommand
 	{
 		return _inputs;
 	}
-	
+
 	@Override
 	public List<T> getOutputs()
 	{
 		return _outputs;
 	}
-	
+
 	public void addOutput(T output)
 	{
 		_outputs.add(output);
 	}
-
-
 
 }
