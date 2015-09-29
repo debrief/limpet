3c0e0b2eb21286a7a9de096d84089772f064f747
Ian Mayo <ian@planetmayo.com>
2015-09-29 10:01:50 +0100

    more data checking

--- info.limpet.test/src/info/limpet/data/TestDynamic.java
+++ info.limpet.test/src/info/limpet/data/TestDynamic.java
@@ -3,6 +3,7 @@ package info.limpet.data;
 import info.limpet.ICollection;
 import info.limpet.ICommand;
 import info.limpet.data.impl.CoreChangeListener;
+import info.limpet.data.impl.QuantityCollection;
 import info.limpet.data.impl.samples.SampleData;
 import info.limpet.data.operations.AddQuantityOperation;
 import info.limpet.data.store.InMemoryStore;
@@ -15,13 +16,15 @@ import junit.framework.TestCase;
 
 public class TestDynamic extends TestCase
 {
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+
+	@SuppressWarnings(
+	{ "unchecked", "rawtypes" })
 	public void testSingleQuantityStats()
 	{
 		// get some data
 		InMemoryStore store = new SampleData().getData();
-			
-		// ok, let's try one that works		
+
+		// ok, let's try one that works
 		List<ICollection> selection = new ArrayList<ICollection>();
 		ICollection speedOne = store.get(SampleData.SPEED_ONE);
 		ICollection speedTwo = store.get(SampleData.SPEED_TWO);
@@ -29,38 +32,39 @@ public class TestDynamic extends TestCase
 		selection.add(speedTwo);
 
 		int storeSize = store.size();
-		
-		Collection<ICommand<?>> actions = new AddQuantityOperation().actionsFor(selection, store );
+
+		Collection<ICommand<?>> actions = new AddQuantityOperation().actionsFor(
+				selection, store);
 		ICommand<?> firstAction = actions.iterator().next();
 		assertEquals("correct action", "Add series", firstAction.getTitle());
-		
+
 		// run the action
 		firstAction.execute();
-		
+
 		// check we have new data
 		assertEquals("new data created", storeSize + 1, store.size());
-		
+
 		// ok, get the new dataset
 		ICollection resSeries = store.get(AddQuantityOperation.SUM_OF_INPUT_SERIES);
 		assertNotNull(resSeries);
-		
+
 		// ok, play about with a change
 		final List<String> events = new ArrayList<String>();
-		CoreChangeListener listener = new CoreChangeListener(){
-
+		CoreChangeListener listener = new CoreChangeListener()
+		{
 			@Override
 			public void dataChanged()
 			{
 				events.add("changed!");
-			}};
+			}
+		};
 		resSeries.addChangeListener(listener);
 		assertEquals("empty at start", 0, events.size());
-		
+
 		// ok, now make a change in one of hte input collections
 		speedOne.fireChanged();
 		assertEquals("change received", 1, events.size());
-		
-		
+
 		// ok, now make another change in one of hte input collections
 		speedOne.fireChanged();
 		assertEquals("second change received", 2, events.size());
@@ -68,18 +72,18 @@ public class TestDynamic extends TestCase
 		selection.clear();
 		selection.add(speedTwo);
 		selection.add(resSeries);
-		
+
 		// ok - now for a further dependent calculation
 		actions = new AddQuantityOperation("output2").actionsFor(selection, store);
 		firstAction = actions.iterator().next();
-		assertEquals("correct action", "Add series", firstAction.getTitle());		
-		
+		assertEquals("correct action", "Add series", firstAction.getTitle());
+
 		// ok, now create the new series
 		firstAction.execute();
-		
+
 		// now check the output changed again
 		events.clear();
-		
+
 		ICollection newResSeries = store.get("output2");
 		assertNotNull("found new series");
 		newResSeries.addChangeListener(listener);
@@ -94,5 +98,11 @@ public class TestDynamic extends TestCase
 		resSeries.fireChanged();
 		assertEquals("change received", 7, events.size());
 
+		// check the data lengths
+		QuantityCollection<?> newResQ = (QuantityCollection<?>) newResSeries;
+		assertEquals("correct elements", 10, newResQ.size());
+		assertEquals("correct elements", 10, speedTwo.size());
+		assertEquals("correct elements", 10, resSeries.size());
+		
 	}
 }
