747966b553860d10facb6f0e641ab3a302c7b203
Haris Peco <snpe@snpe.rs>
2015-10-01 00:24:39 +0200

    Issue #60 - Update perspective layout

--- info.limpet.rcp/src/info/limpet/rcp/product/Perspective.java
+++ info.limpet.rcp/src/info/limpet/rcp/product/Perspective.java
@@ -17,21 +17,21 @@ public class Perspective implements IPerspectiveFactory
 	{
 		final String editorArea = layout.getEditorArea();
 
-		final IFolderLayout topLeft = layout.createFolder("topLeft",
-				IPageLayout.LEFT, 0.25f, editorArea);
-
+		final IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 0.3f, editorArea);
 		topLeft.addView(IPageLayout.ID_RES_NAV);
-		final IFolderLayout bottom = layout
-				.createFolder("bottom", IPageLayout.BOTTOM, 0.6f, editorArea);
 		
-		bottom.addView(AnalysisView.ID);
+		final IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.4f, "topLeft");
+		bottomLeft.addView(IPageLayout.ID_PROP_SHEET);
+				
+		final IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.5f, editorArea);
 		bottom.addView(DataFrequencyView.ID);
-		bottom.addView(IPageLayout.ID_PROP_SHEET);
 
-		final IFolderLayout bottom2 = layout
-				.createFolder("bottom", IPageLayout.BOTTOM, 0.6f, editorArea);
-		bottom2.addView(XyPlotView.ID);
+		final IFolderLayout bottomRight = layout.createFolder("bottomRight", IPageLayout.RIGHT, 0.5f, "bottom");
+		bottomRight.addView(XyPlotView.ID);
 
+		final IFolderLayout topRight = layout.createFolder("topRight", IPageLayout.RIGHT, 0.6f, editorArea);
+		topRight.addView(AnalysisView.ID);
+		
 		
 		// and our view shortcuts
 		layout.addShowViewShortcut(IPageLayout.ID_RES_NAV);
