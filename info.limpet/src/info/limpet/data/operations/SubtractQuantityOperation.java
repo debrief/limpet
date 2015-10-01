98e1971cc69708896dc1c8fbebd8703455a116a2
Ian Mayo <ian@planetmayo.com>
2015-10-01 11:03:50 +0100

    minor optimisation - fails quicker

--- info.limpet/src/info/limpet/data/operations/SubtractQuantityOperation.java
+++ info.limpet/src/info/limpet/data/operations/SubtractQuantityOperation.java
@@ -64,8 +64,8 @@ public class SubtractQuantityOperation<Q extends Quantity<Q>> implements
 
 	private boolean appliesTo(List<ICollection> selection)
 	{
-		boolean allQuantity = aTests.allQuantity(selection);
 		boolean onlyTwoCollections = aTests.exactNumber(selection, 2);
+		boolean allQuantity = aTests.allQuantity(selection);
 		boolean equalLength = aTests.allEqualLength(selection);
 		boolean equalDimensions = aTests.allEqualDimensions(selection);
 		return (allQuantity && equalLength && equalDimensions && onlyTwoCollections);
@@ -119,7 +119,7 @@ public class SubtractQuantityOperation<Q extends Quantity<Q>> implements
 			// ok, done
 			List<ICollection> res = new ArrayList<ICollection>();
 			res.add(target);
-			getStore().add(res);
+			getStore().addAll(res);
 		}
 
 		@SuppressWarnings(
