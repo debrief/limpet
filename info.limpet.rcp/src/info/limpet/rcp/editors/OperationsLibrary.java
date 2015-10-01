7c666ff2caf087608153bc02527404e3a5b572a3
Ian Mayo <ian@planetmayo.com>
2015-10-01 09:16:30 +0100

    add conversions to operations library

--- info.limpet.rcp/src/info/limpet/rcp/editors/OperationsLibrary.java
+++ info.limpet.rcp/src/info/limpet/rcp/editors/OperationsLibrary.java
@@ -2,11 +2,13 @@ package info.limpet.rcp.editors;
 
 import info.limpet.ICollection;
 import info.limpet.IOperation;
+import info.limpet.data.impl.samples.StockTypes;
 import info.limpet.data.operations.AddQuantityOperation;
 import info.limpet.data.operations.DeleteCollectionOperation;
 import info.limpet.data.operations.DivideQuantityOperation;
 import info.limpet.data.operations.MultiplyQuantityOperation;
 import info.limpet.data.operations.SubtractQuantityOperation;
+import info.limpet.data.operations.UnitConversionOperation;
 import info.limpet.rcp.analysis_view.AnalysisView;
 import info.limpet.rcp.data_frequency.DataFrequencyView;
 import info.limpet.rcp.operations.ShowInNamedView;
@@ -16,17 +18,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import tec.units.ri.unit.Units;
+
 public class OperationsLibrary
 {
 	public static HashMap<String, List<IOperation<?>>> getOperations()
 	{
 		HashMap<String, List<IOperation<?>>> res = new HashMap<String, List<IOperation<?>>>();
 
-		List<IOperation<?>> ops = new ArrayList<IOperation<?>>();
-		ops.add(new MultiplyQuantityOperation());
-		ops.add(new AddQuantityOperation<>());
-		ops.add(new SubtractQuantityOperation<>());
-		ops.add(new DivideQuantityOperation());
+		List<IOperation<?>> arithmetic = new ArrayList<IOperation<?>>();
+		arithmetic.add(new MultiplyQuantityOperation());
+		arithmetic.add(new AddQuantityOperation<>());
+		arithmetic.add(new SubtractQuantityOperation<>());
+		arithmetic.add(new DivideQuantityOperation());
+
+		List<IOperation<?>> conversions = new ArrayList<IOperation<?>>();
+		conversions.add(new UnitConversionOperation(Units.METRE));
+		conversions.add(new UnitConversionOperation(Units.SECOND));
+		conversions.add(new UnitConversionOperation(Units.MINUTE));
+		conversions.add(new UnitConversionOperation(Units.METRES_PER_SECOND));
+		conversions
+				.add(new UnitConversionOperation(Units.METRES_PER_SQUARE_SECOND));
+		conversions.add(new UnitConversionOperation(Units.CELSIUS));
+		conversions.add(new UnitConversionOperation(Units.RADIAN));
+		conversions.add(new UnitConversionOperation(StockTypes.DEGREE_ANGLE));
 
 		List<IOperation<?>> admin = new ArrayList<IOperation<?>>();
 		admin.add(new DeleteCollectionOperation());
@@ -44,7 +59,8 @@ public class OperationsLibrary
 				DataFrequencyView.ID));
 		analysis.add(new ShowInNamedView("Show in Analysis View", AnalysisView.ID));
 
-		res.put("Arithmetic", ops);
+		res.put("Arithmetic", arithmetic);
+		res.put("Conversions", conversions);
 		res.put("Administration", admin);
 		res.put("Analysis", analysis);
 		return res;
