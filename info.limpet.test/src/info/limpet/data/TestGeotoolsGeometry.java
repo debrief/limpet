b00f186367f48fa6078efa229521cf96d86af218
Haris Peco <snpe@snpe.rs>
2015-10-04 00:01:10 +0200

    Issue #63 - Removing JSR-363

--- info.limpet.test/src/info/limpet/data/TestGeotoolsGeometry.java
+++ info.limpet.test/src/info/limpet/data/TestGeotoolsGeometry.java
@@ -1,6 +1,13 @@
 package info.limpet.data;
 
+import org.geotools.factory.Hints;
+import org.geotools.geometry.GeometryBuilder;
+import org.geotools.geometry.GeometryFactoryFinder;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.junit.Assert;
 import org.opengis.geometry.Geometry;
+import org.opengis.geometry.primitive.Point;
+import org.opengis.geometry.primitive.PrimitiveFactory;
 
 import info.limpet.data.impl.TemporalObjectCollection;
 import junit.framework.TestCase;
@@ -13,6 +20,18 @@ public class TestGeotoolsGeometry extends TestCase
 		TemporalObjectCollection<Geometry> locations = new TemporalObjectCollection<Geometry>(
 				"test");
 		assertNotNull(locations);
+	}
+	
+	public void testCreatePoint()
+	{
+		GeometryBuilder builder = new GeometryBuilder( DefaultGeographicCRS.WGS84 );
+		Point point = builder.createPoint( 48.44, -123.37 );
+		Assert.assertNotNull(point);
+		
+		Hints hints = new Hints( Hints.CRS, DefaultGeographicCRS.WGS84 );
+		PrimitiveFactory primitiveFactory = GeometryFactoryFinder.getPrimitiveFactory( hints );
+		Point point2 = primitiveFactory.createPoint(  new double[]{48.44, -123.37} );
+		Assert.assertNotNull(point2);
 	}
 
 }
