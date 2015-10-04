3c09bc5a0489752975f916f4e1346837f6ae7caa
ianmayo <ian@planetmayo.com>
2015-10-04 09:08:24 +0100

    first steps to implement new collections

--- info.limpet/src/info/limpet/App.java
+++ info.limpet/src/info/limpet/App.java
@@ -1,15 +1,60 @@
 package info.limpet;
 
-import javax.measure.Quantity;
+import static javax.measure.unit.SI.GRAM;
+import static javax.measure.unit.SI.KILOGRAM;
+
+import javax.measure.Measurable;
+import javax.measure.Measure;
 import javax.measure.quantity.Mass;
+import javax.measure.quantity.Quantity;
+import javax.measure.unit.NonSI;
+import javax.measure.unit.Unit;
 
-import systems.uom.common.US;
-import tec.units.ri.quantity.Quantities;
-import tec.units.ri.unit.Units;
+public class App
+{
+	public static void main(String[] args)
+	{
+		Measure<Double, Mass> mass = Measure.valueOf(50.0, KILOGRAM);
+		System.out.println(mass.to(NonSI.POUND));
 
-public class App {
-	public static void main(String[] args) {
-		Quantity<Mass> mass = Quantities.getQuantity(70, Units.KILOGRAM);
-		System.out.println(mass.to(US.POUND).getValue());
+		
+		// try to store some data
+		IQuantityCollection<Mass> qc = new QuantityCollection<Mass>(
+				KILOGRAM);
+		qc.add(Measure.valueOf(24, GRAM));
+		qc.add(Measure.valueOf(4, KILOGRAM));
+
 	}
+
+	public static interface IQuantityCollection<Q extends Quantity>
+	{
+		void add(Measurable<Q> item);
+		void add(double newVal);
+	}
+
+	public static class QuantityCollection<Q extends Quantity>
+			implements IQuantityCollection<Q>
+	{
+		final Unit<Q> _units;
+
+		public QuantityCollection(Unit<Q> units)
+		{
+			_units = units;
+		}
+
+		@Override
+		public void add(Measurable<Q> item)
+		{
+			System.out.println("adding:" + item.toString());
+		}
+
+		@Override
+		public void add(double newVal)
+		{
+			add(Measure.valueOf(newVal, _units));
+		}
+		
+		
+	};
+
 }
