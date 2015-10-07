dbfd3b145c2a17f595ce0f48867d5faee259f4e8
Ian Mayo <ian@planetmayo.com>
2015-10-07 12:49:48 +0100

    do write & read in one test - we can't rely on the tests otherwise

--- info.limpet.test/src/info/limpet/data/TestPersistence.java
+++ info.limpet.test/src/info/limpet/data/TestPersistence.java
@@ -1,33 +1,59 @@
 package info.limpet.data;
 
+import java.io.File;
 import java.io.IOException;
 
 import info.limpet.IStore;
 import info.limpet.data.impl.samples.SampleData;
 import info.limpet.data.persistence.xml.XStreamHandler;
+import info.limpet.data.store.InMemoryStore;
 import junit.framework.TestCase;
 
 public class TestPersistence extends TestCase
 {
-	
-	public void testSaveSampleData()
+
+	public void testSaveThenLoadSampleData()
 	{
-		IStore data = new SampleData().getData(20);
+		InMemoryStore data = new SampleData().getData(20);
+		final long storeSize = data.size();
+		final String fileName = "testtemp.lap";
+
+		// clear the test file
+		File testF = new File(fileName);
+		if (testF.exists())
+		{
+			testF.delete();
+		}
+		
+		assertTrue("file doesn't exist", !testF.exists());
+
 		try
 		{
-			new XStreamHandler().save(data, "testtemp.lap");
+			new XStreamHandler().save(data, fileName);
 		}
 		catch (IOException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	public void testLoadSampleData()
-	{
-		IStore data = new XStreamHandler().load("test.lap");
-		System.out.println(data);
+
+		assertTrue("file created", testF.exists());
+
+		IStore data2 = new XStreamHandler().load(fileName);
+		assertNotNull("found store", data2);
+		if (data2 instanceof InMemoryStore)
+		{
+			InMemoryStore ims = (InMemoryStore) data2;
+			assertEquals("correct num of objects", storeSize, ims.size());
+		}
+		
+		// do a bit of tidying up
+//		if (testF.exists())
+//		{
+//			testF.delete();
+//		}
+
+
 	}
 
 }
