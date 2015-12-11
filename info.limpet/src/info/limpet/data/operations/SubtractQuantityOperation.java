35b9a76f31751b6bc9aeefde9df296e998b7e8ea
Ian Mayo <ian@deepbluec.com>
2015-12-11 12:56:19 +0000

    include Viral's fixes

--- info.limpet/src/info/limpet/data/operations/SubtractQuantityOperation.java
+++ info.limpet/src/info/limpet/data/operations/SubtractQuantityOperation.java
@@ -1,19 +1,18 @@
 package info.limpet.data.operations;
 
-import info.limpet.ICollection;
-import info.limpet.ICommand;
-import info.limpet.IOperation;
-import info.limpet.IQuantityCollection;
-import info.limpet.IStore;
-import info.limpet.IStore.IStoreItem;
-import info.limpet.ITemporalQuantityCollection;
-import info.limpet.ITemporalQuantityCollection.InterpMethod;
-
 import java.util.Collection;
 import java.util.List;
 
 import javax.measure.Measurable;
 import javax.measure.quantity.Quantity;
+
+import info.limpet.ICollection;
+import info.limpet.ICommand;
+import info.limpet.IOperation;
+import info.limpet.IQuantityCollection;
+import info.limpet.IStore;
+import info.limpet.ITemporalQuantityCollection;
+import info.limpet.ITemporalQuantityCollection.InterpMethod;
 
 public class SubtractQuantityOperation<Q extends Quantity> extends
 		CoreQuantityOperation<Q> implements IOperation<IQuantityCollection<Q>>
@@ -48,8 +47,6 @@ public class SubtractQuantityOperation<Q extends Quantity> extends
 		}
 	}
 
-	@SuppressWarnings(
-	{ "rawtypes", "unchecked" })
 	@Override
 	protected void addInterpolatedCommands(
 			List<IQuantityCollection<Q>> selection, IStore destination,
@@ -63,20 +60,19 @@ public class SubtractQuantityOperation<Q extends Quantity> extends
 			ICollection item2 = (ICollection) selection.get(1);
 
 			String oName = item2.getName() + " from " + item1.getName();
-			ICommand<? extends IStoreItem> newC = new SubtractQuantityValues(
+			ICommand<IQuantityCollection<Q>> newC = new SubtractQuantityValues<Quantity>(
 					"Subtract " + item2.getName() + " from " + item1.getName(), oName,
 					selection, item1, item2, destination, longest);
-			res.add((ICommand<IQuantityCollection<Q>>) newC);
+
+			res.add(newC);
 			oName = item1.getName() + " from " + item2.getName();
-			newC = new SubtractQuantityValues("Subtract " + item1.getName()
+			newC = new SubtractQuantityValues<Quantity>("Subtract " + item1.getName()
 					+ " from " + item2.getName(), oName, selection, item2, item1,
 					destination, longest);
-			res.add((ICommand<IQuantityCollection<Q>>) newC);
+			res.add(newC);
 		}
 	}
 
-	@SuppressWarnings(
-	{ "unchecked", "rawtypes" })
 	protected void addIndexedCommands(List<IQuantityCollection<Q>> selection,
 			IStore destination, Collection<ICommand<IQuantityCollection<Q>>> res)
 	{
@@ -84,14 +80,16 @@ public class SubtractQuantityOperation<Q extends Quantity> extends
 		ICollection item2 = (ICollection) selection.get(1);
 
 		String oName = item2.getName() + " from " + item1.getName();
-		ICommand<? extends IStoreItem> newC = new SubtractQuantityValues(
+		ICommand<IQuantityCollection<Q>> newC = new SubtractQuantityValues<Quantity>(
 				"Subtract " + item2.getName() + " from " + item1.getName(), oName,
 				selection, item1, item2, destination);
-		res.add((ICommand<IQuantityCollection<Q>>) newC);
+
+		res.add(newC);
 		oName = item1.getName() + " from " + item2.getName();
-		newC = new SubtractQuantityValues("Subtract " + item1.getName() + " from "
-				+ item2.getName(), oName, selection, item2, item1, destination);
-		res.add((ICommand<IQuantityCollection<Q>>) newC);
+		newC = new SubtractQuantityValues<Quantity>("Subtract " + item1.getName()
+				+ " from " + item2.getName(), oName, selection, item2, item1,
+				destination);
+		res.add(newC);
 	}
 
 	public class SubtractQuantityValues<T extends Quantity> extends
@@ -107,7 +105,6 @@ public class SubtractQuantityOperation<Q extends Quantity> extends
 			this(title, outputName, selection, item1, item2, store, null);
 		}
 
-		@SuppressWarnings("unchecked")
 		public SubtractQuantityValues(String title, String outputName,
 				List<IQuantityCollection<Q>> selection, ICollection item1,
 				ICollection item2, IStore store,
@@ -129,12 +126,11 @@ public class SubtractQuantityOperation<Q extends Quantity> extends
 			return runningTotal;
 		}
 
-		@SuppressWarnings("unchecked")
 		@Override
 		protected Double calcThisInterpolatedElement(long time)
 		{
-			ITemporalQuantityCollection<Q> tqc1 = (ITemporalQuantityCollection<Q>) _item1;
-			ITemporalQuantityCollection<Q> tqc2 = (ITemporalQuantityCollection<Q>) _item2;
+			ITemporalQuantityCollection<T> tqc1 = (ITemporalQuantityCollection<T>) _item1;
+			ITemporalQuantityCollection<T> tqc2 = (ITemporalQuantityCollection<T>) _item2;
 
 			final Measurable<T> thisValue = (Measurable<T>) tqc1.interpolateValue(
 					time, InterpMethod.Linear);
@@ -147,10 +143,7 @@ public class SubtractQuantityOperation<Q extends Quantity> extends
 			double otherD = 0;
 			if (otherValue != null)
 				otherD = otherValue.doubleValue(_item1.getUnits());
-
 			return thisD - otherD;
 		}
-
 	}
-
 }
