a72d7ee593619e840e1ad38eb1439cdd00162502
Ian Mayo <ian@deepbluec.com>
2015-12-14 12:47:10 +0000

    apply formatting, to match standard:  https://github.com/debrief/limpet/blob/master/code_format.xml

--- info.limpet.rcp/src/info/limpet/rcp/data_provider/data/ReflectivePropertySource.java
+++ info.limpet.rcp/src/info/limpet/rcp/data_provider/data/ReflectivePropertySource.java
@@ -25,134 +25,133 @@ public class ReflectivePropertySource implements IPropertySource
 
 	public ReflectivePropertySource(Object object)
 	{
-	this.object = object;
+		this.object = object;
 	}
 
 	@Override
 	public Object getEditableValue()
 	{
-	return object;
+		return object;
 	}
 
 	@Override
 	public IPropertyDescriptor[] getPropertyDescriptors()
 	{
-	if (propertyDescriptors == null)
-	{
-		initPropertyDescriptors(object.getClass());
-	}
-	return propertyDescriptors;
+		if (propertyDescriptors == null)
+		{
+			initPropertyDescriptors(object.getClass());
+		}
+		return propertyDescriptors;
 	}
 
 	private void initPropertyDescriptors(Class<?> cls)
 	{
 
-	descriptorPerProperty = new HashMap<String, PropertyDescriptor>();
+		descriptorPerProperty = new HashMap<String, PropertyDescriptor>();
 
-	List<IPropertyDescriptor> result = new ArrayList<IPropertyDescriptor>();
+		List<IPropertyDescriptor> result = new ArrayList<IPropertyDescriptor>();
 
-	try
-	{
-		PropertyDescriptor[] beanPropertyDescriptors = Introspector
-			.getBeanInfo(cls).getPropertyDescriptors();
-		for (PropertyDescriptor beanPropertyDescriptor : beanPropertyDescriptors)
+		try
 		{
-		UIProperty annotation = beanPropertyDescriptor.getReadMethod()
-			.getAnnotation(UIProperty.class);
-		if (annotation != null)
+			PropertyDescriptor[] beanPropertyDescriptors = Introspector.getBeanInfo(
+					cls).getPropertyDescriptors();
+			for (PropertyDescriptor beanPropertyDescriptor : beanPropertyDescriptors)
+			{
+				UIProperty annotation = beanPropertyDescriptor.getReadMethod()
+						.getAnnotation(UIProperty.class);
+				if (annotation != null)
+				{
+
+					org.eclipse.ui.views.properties.PropertyDescriptor descriptor = null;
+					String propName = annotation.name();
+					String propCategory = annotation.category();
+					if (beanPropertyDescriptor.getWriteMethod() != null)
+					{
+						if (beanPropertyDescriptor.getPropertyType().equals(String.class))
+						{
+							descriptor = new TextPropertyDescriptor(
+									beanPropertyDescriptor.getName(), propName);
+						}
+						else
+						{
+							// TODO: handle other property types, such as
+							// number, boolean,
+							// complex types, etc
+						}
+					}
+					else
+					{
+						// read only descriptor
+						descriptor = new org.eclipse.ui.views.properties.PropertyDescriptor(
+								beanPropertyDescriptor.getName(), propName);
+					}
+
+					if (descriptor != null)
+					{
+						descriptor.setCategory(propCategory);
+						result.add(descriptor);
+
+						descriptorPerProperty.put(beanPropertyDescriptor.getName(),
+								beanPropertyDescriptor);
+					}
+
+				}
+			}
+		}
+		catch (IntrospectionException e)
 		{
-
-			org.eclipse.ui.views.properties.PropertyDescriptor descriptor = null;
-			String propName = annotation.name();
-			String propCategory = annotation.category();
-			if (beanPropertyDescriptor.getWriteMethod() != null)
-			{
-			if (beanPropertyDescriptor.getPropertyType().equals(String.class))
-			{
-				descriptor = new TextPropertyDescriptor(
-					beanPropertyDescriptor.getName(), propName);
-			}
-			else
-			{
-				// TODO: handle other property types, such as
-				// number, boolean,
-				// complex types, etc
-			}
-			}
-			else
-			{
-			// read only descriptor
-			descriptor = new org.eclipse.ui.views.properties.PropertyDescriptor(
-				beanPropertyDescriptor.getName(), propName);
-			}
-
-			if (descriptor != null)
-			{
-			descriptor.setCategory(propCategory);
-			result.add(descriptor);
-
-			descriptorPerProperty.put(beanPropertyDescriptor.getName(),
-				beanPropertyDescriptor);
-			}
+			Activator.logError(Status.ERROR,
+					"Could not load property descriptors for class " + cls.getName(), e);
 
 		}
-		}
-	}
-	catch (IntrospectionException e)
-	{
-		Activator.logError(Status.ERROR,
-			"Could not load property descriptors for class " + cls.getName(),
-			e);
 
-	}
-
-	propertyDescriptors = result
-		.toArray(new IPropertyDescriptor[result.size()]);
+		propertyDescriptors = result
+				.toArray(new IPropertyDescriptor[result.size()]);
 	}
 
 	@Override
 	public Object getPropertyValue(Object id)
 	{
-	PropertyDescriptor descriptor = descriptorPerProperty.get(id);
-	try
-	{
-		return descriptor.getReadMethod().invoke(object);
-	}
-	catch (Exception e)
-	{
-		Activator.logError(Status.ERROR,
-			"Could not retrive value for property " + id, e);
-		return null;
-	}
+		PropertyDescriptor descriptor = descriptorPerProperty.get(id);
+		try
+		{
+			return descriptor.getReadMethod().invoke(object);
+		}
+		catch (Exception e)
+		{
+			Activator.logError(Status.ERROR, "Could not retrive value for property "
+					+ id, e);
+			return null;
+		}
 	}
 
 	@Override
 	public boolean isPropertySet(Object id)
 	{
-	// TODO Auto-generated method stub
-	return false;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public void resetPropertyValue(Object id)
 	{
-	// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void setPropertyValue(Object id, Object value)
 	{
-	PropertyDescriptor descriptor = descriptorPerProperty.get(id);
-	try
-	{
-		descriptor.getWriteMethod().invoke(object, value);
-	}
-	catch (Exception e)
-	{
-		Activator.logError(Status.ERROR,
-			"Could not set value for property " + id, e);
-	}
+		PropertyDescriptor descriptor = descriptorPerProperty.get(id);
+		try
+		{
+			descriptor.getWriteMethod().invoke(object, value);
+		}
+		catch (Exception e)
+		{
+			Activator.logError(Status.ERROR,
+					"Could not set value for property " + id, e);
+		}
 	}
 
 }
