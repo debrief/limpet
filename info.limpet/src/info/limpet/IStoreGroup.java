f273555a045123a646cc3498bb7d69e6e5288711
Ian Mayo <ian@deepbluec.com>
2015-12-17 10:35:32 +0000

    remove Java8 specific code

--- info.limpet/src/info/limpet/IStoreGroup.java
+++ info.limpet/src/info/limpet/IStoreGroup.java
@@ -7,9 +7,5 @@ import info.limpet.IStore.IStoreItem;
 
 public interface IStoreGroup extends IStoreItem, Collection<IStoreItem>, IChangeListener
 {
-  /* Note: clarify which splititerator we want to use, to overcome compiler warning)
-   *
-   */
-  public Spliterator<IStoreItem> spliterator();
   
 }
