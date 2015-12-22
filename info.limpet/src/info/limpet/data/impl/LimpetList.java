d01624793736f937f59873b2a83663a26482bf78
Haris Peco <snpe@snpe.rs>
2015-12-22 15:02:15 +0100

    Issue #129 - Adding comment to LimpetList/TimesList

--- info.limpet/src/info/limpet/data/impl/LimpetList.java
+++ info.limpet/src/info/limpet/data/impl/LimpetList.java
@@ -1,7 +1,14 @@
 package info.limpet.data.impl;
 
 import java.util.ArrayList;
-
+/**
+ * Marker class which enable to recognize collection of <code>javax.measure.Measure</code>.
+ *
+ * @author snpe
+ *
+ * @see ArrayList
+ * @see ObjectCollection
+ */
 public class LimpetList<E> extends ArrayList<E>
 {
 
--- info.limpet/src/info/limpet/data/impl/TimesList.java
+++ info.limpet/src/info/limpet/data/impl/TimesList.java
@@ -2,6 +2,15 @@ package info.limpet.data.impl;
 
 import java.util.ArrayList;
 
+/**
+ * Marker class which enable to recognize temporal/times collections.
+ *
+ * @author snpe
+ *
+ * @see ArrayList
+ * @see TemporalObjectCollection
+ *
+ */
 public class TimesList<E> extends ArrayList<Long>
 {
 
