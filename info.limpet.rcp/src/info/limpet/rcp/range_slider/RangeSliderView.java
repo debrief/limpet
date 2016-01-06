0b3b815066269a1889861f6a817021dc32fa1758
Ian Mayo <ian@deepbluec.com>
2016-01-06 15:29:31 +0000

    trim license length

--- info.limpet.rcp/src/info/limpet/rcp/range_slider/RangeSliderView.java
+++ info.limpet.rcp/src/info/limpet/rcp/range_slider/RangeSliderView.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*****************************************************************************
  *  Limpet - the Lightweight InforMation ProcEssing Toolkit
  *  http://limpet.info
  *
@@ -11,7 +11,7 @@
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *******************************************************************************/
+ *****************************************************************************/
 package info.limpet.rcp.range_slider;
 
 import info.limpet.IChangeListener;
