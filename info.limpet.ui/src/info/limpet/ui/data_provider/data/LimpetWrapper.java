d334d7a2f336219f148f1bd22f3e08557711663f
Ian Mayo <ian@deepbluec.com>
2016-01-13 11:28:08 +0000

    hotfix - code tidying

--- info.limpet.ui/src/info/limpet/ui/data_provider/data/LimpetWrapper.java
+++ info.limpet.ui/src/info/limpet/ui/data_provider/data/LimpetWrapper.java
@@ -22,17 +22,17 @@ package info.limpet.ui.data_provider.data;
  */
 public interface LimpetWrapper
 {
-	/**
-	 * retrieve the parent of the current object
-	 * 
-	 * @return
-	 */
-	LimpetWrapper getParent();
+  /**
+   * retrieve the parent of the current object
+   * 
+   * @return the parent of the current object (to let us walk the tree)
+   */
+  LimpetWrapper getParent();
 
-	/**
-	 * retrieve the pure limpet object that this instance is wrapping
-	 * 
-	 * @return
-	 */
-	Object getSubject();
+  /**
+   * retrieve the pure limpet object that this instance is wrapping
+   * 
+   * @return the wrapped entity
+   */
+  Object getSubject();
 }
