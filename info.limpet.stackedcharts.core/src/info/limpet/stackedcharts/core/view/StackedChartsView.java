1e8eedcb5c396c6de61836be381438a19359d1da
Anuradha Gunasekara <theanuradha@gmail.com>
2016-05-16 10:39:24 +0530

    added File drop support to load chart model

--- info.limpet.stackedcharts.core/src/info/limpet/stackedcharts/core/view/StackedChartsView.java
+++ info.limpet.stackedcharts.core/src/info/limpet/stackedcharts/core/view/StackedChartsView.java
@@ -2,14 +2,28 @@ package info.limpet.stackedcharts.core.view;
 
 import info.limpet.stackedcharts.model.ChartSet;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.nebula.effects.stw.Transition;
 import org.eclipse.nebula.effects.stw.TransitionManager;
 import org.eclipse.nebula.effects.stw.Transitionable;
 import org.eclipse.nebula.effects.stw.transitions.CubicRotationTransition;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DropTarget;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.swt.dnd.DropTargetListener;
+import org.eclipse.swt.dnd.FileTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FillLayout;
@@ -17,6 +31,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.part.ViewPart;
@@ -28,7 +43,7 @@ public class StackedChartsView extends ViewPart
 
   public static final int CHART_VIEW = 1;
   public static final int EDIT_VIEW = 2;
-  
+
   public static final String ID = "info.limpet.StackedChartsView";
 
   private StackedPane stackedPane;
@@ -47,8 +62,11 @@ public class StackedChartsView extends ViewPart
     stackedPane.add(EDIT_VIEW, createEditView());
     selectView(CHART_VIEW);
     contributeToActionBars();
-    
-    // see  https://github.com/debrief/limpet/issues/265
+
+    // Drop Support for *.stackedcharts
+    connectFileDropSupport(stackedPane);
+
+    // see https://github.com/debrief/limpet/issues/265
     if (!System.getProperty("os.name").toLowerCase().contains("mac"))
     {
       transitionManager = new TransitionManager(new Transitionable()
@@ -75,7 +93,7 @@ public class StackedChartsView extends ViewPart
 
         public void setSelection(int index)
         {
-          stackedPane.showPane(index,false);
+          stackedPane.showPane(index, false);
         }
 
         public double getDirection(int toIndex, int fromIndex)
@@ -84,10 +102,134 @@ public class StackedChartsView extends ViewPart
               : Transition.DIR_LEFT;
         }
       });
-      //new SlideTransition(_tm)
-      transitionManager.setTransition(new CubicRotationTransition(transitionManager));
+      // new SlideTransition(_tm)
+      transitionManager.setTransition(new CubicRotationTransition(
+          transitionManager));
     }
 
+  }
+
+  protected void connectFileDropSupport(Control compoent)
+  {
+    DropTarget target =
+        new DropTarget(compoent, DND.DROP_MOVE | DND.DROP_COPY
+            | DND.DROP_DEFAULT);
+    final FileTransfer fileTransfer = FileTransfer.getInstance();
+    target.setTransfer(new Transfer[]
+    {fileTransfer});
+    target.addDropListener(new DropTargetListener()
+    {
+
+      @Override
+      public void dropAccept(DropTargetEvent event)
+      {
+        // TODO Auto-generated method stub
+
+      }
+
+      @Override
+      public void drop(DropTargetEvent event)
+      {
+
+        if (fileTransfer.isSupportedType(event.currentDataType))
+        {
+          String[] files = (String[]) event.data;
+          
+            // *.stackedcharts
+            if (files.length == 1 && files[0].endsWith("stackedcharts"))
+            {
+              File file = new File(files[0]);
+              Resource resource =
+                  new ResourceSetImpl().createResource(URI.createURI(file
+                      .toURI().toString()));
+              try
+              {
+                resource.load(new HashMap<>());
+                ChartSet chartsSet = (ChartSet) resource.getContents().get(0);
+                setModel(chartsSet);
+              }
+              catch (IOException e)
+              {
+                e.printStackTrace();
+                MessageDialog.openError(
+                    Display.getCurrent().getActiveShell(), "Error", e
+                        .getMessage());
+                
+              }
+              
+            }
+          }
+        
+
+      }
+
+      @Override
+      public void dragOver(DropTargetEvent event)
+      {
+        
+
+      }
+
+      @Override
+      public void dragOperationChanged(DropTargetEvent event)
+      {
+        if (event.detail == DND.DROP_DEFAULT)
+        {
+          if ((event.operations & DND.DROP_COPY) != 0)
+          {
+            event.detail = DND.DROP_COPY;
+          }
+          else
+          {
+            event.detail = DND.DROP_NONE;
+          }
+        }
+        if (fileTransfer.isSupportedType(event.currentDataType))
+        {
+          if (event.detail != DND.DROP_COPY)
+          {
+            event.detail = DND.DROP_NONE;
+          }
+        }
+
+      }
+
+      @Override
+      public void dragLeave(DropTargetEvent event)
+      {
+
+      }
+
+      @Override
+      public void dragEnter(DropTargetEvent event)
+      {
+        if (event.detail == DND.DROP_DEFAULT)
+        {
+          if ((event.operations & DND.DROP_COPY) != 0)
+          {
+            event.detail = DND.DROP_COPY;
+          }
+          else
+          {
+            event.detail = DND.DROP_NONE;
+          }
+        }
+        for (int i = 0; i < event.dataTypes.length; i++)
+        {
+          if (fileTransfer.isSupportedType(event.dataTypes[i]))
+          {
+            event.currentDataType = event.dataTypes[i];
+            // files should only be copied
+            if (event.detail != DND.DROP_COPY)
+            {
+              event.detail = DND.DROP_NONE;
+            }
+            break;
+          }
+        }
+
+      }
+    });
   }
 
   public void selectView(int view)
@@ -98,37 +240,39 @@ public class StackedChartsView extends ViewPart
     }
 
   }
-  
+
   public void setModel(ChartSet charts)
   {
     // remove any existing base items
-    if(chartHolder != null)
+    if (chartHolder != null)
     {
-      for (Control control : chartHolder.getChildren()) 
+      for (Control control : chartHolder.getChildren())
       {
         control.dispose();
       }
     }
-      
+
     // and now repopulate
-    JFreeChart chart =  new ChartBuilder(charts).build();
+    JFreeChart chart = new ChartBuilder(charts).build();
     @SuppressWarnings("unused")
-    ChartComposite  _chartComposite = new ChartComposite(chartHolder, SWT.NONE, chart, true)
-    {
-      @Override
-      public void mouseUp(MouseEvent event)
-      {
-        super.mouseUp(event);
-        JFreeChart c = getChart();
-        if (c != null)
+    ChartComposite _chartComposite =
+        new ChartComposite(chartHolder, SWT.NONE, chart, true)
         {
-          c.setNotify(true); // force redraw
-        }
-      }
-    };
-    
+          @Override
+          public void mouseUp(MouseEvent event)
+          {
+            super.mouseUp(event);
+            JFreeChart c = getChart();
+            if (c != null)
+            {
+              c.setNotify(true); // force redraw
+            }
+          }
+        };
+
     chartHolder.pack(true);
     chartHolder.getParent().layout();
+    selectView(CHART_VIEW);
   }
 
   protected Control createChartView()
@@ -136,30 +280,30 @@ public class StackedChartsView extends ViewPart
 
     chartHolder = new Composite(stackedPane, SWT.NONE);
     chartHolder.setLayout(new FillLayout());
-   
-//    JFreeChart chart = createChart();
-//    @SuppressWarnings("unused")
-//    ChartComposite  _chartComposite = new ChartComposite(chartHolder, SWT.NONE, chart, true)
-//    {
-//      @Override
-//      public void mouseUp(MouseEvent event)
-//      {
-//        super.mouseUp(event);
-//        JFreeChart c = getChart();
-//        if (c != null)
-//        {
-//          c.setNotify(true); // force redraw
-//        }
-//      }
-//    };
-    
+
+    // JFreeChart chart = createChart();
+    // @SuppressWarnings("unused")
+    // ChartComposite _chartComposite = new ChartComposite(chartHolder, SWT.NONE, chart, true)
+    // {
+    // @Override
+    // public void mouseUp(MouseEvent event)
+    // {
+    // super.mouseUp(event);
+    // JFreeChart c = getChart();
+    // if (c != null)
+    // {
+    // c.setNotify(true); // force redraw
+    // }
+    // }
+    // };
+
     return chartHolder;
   }
 
-//  private JFreeChart createChart()
-//  {
-//    return new ChartBuilder(ChartBuilder.createDummyModel()).build();
-//  }
+  // private JFreeChart createChart()
+  // {
+  // return new ChartBuilder(ChartBuilder.createDummyModel()).build();
+  // }
 
   protected Control createEditView()
   {
