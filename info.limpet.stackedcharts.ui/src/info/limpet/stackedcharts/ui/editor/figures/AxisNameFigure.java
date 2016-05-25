d53138a5110d550b42a64f3b415742e8dcfb3295
Anuradha Gunasekara <theanuradha@gmail.com>
2016-05-25 18:28:31 +0530

    ui changes on font bold

--- info.limpet.stackedcharts.ui/src/info/limpet/stackedcharts/ui/editor/figures/AxisNameFigure.java
+++ info.limpet.stackedcharts.ui/src/info/limpet/stackedcharts/ui/editor/figures/AxisNameFigure.java
@@ -6,9 +6,14 @@ import org.eclipse.draw2d.FlowLayout;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.draw2d.RectangleFigure;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.widgets.Display;
 
 public class AxisNameFigure extends RectangleFigure
 {
+  private static volatile Font boldFont;
   private VerticalLabel verticalLabel;
 
   public AxisNameFigure(ActionListener deleteHandler)
@@ -17,13 +22,15 @@ public class AxisNameFigure extends RectangleFigure
     FlowLayout layout = new FlowLayout(false);
     layout.setMinorAlignment(FlowLayout.ALIGN_CENTER);
     setLayoutManager(layout);
-    
+
     Button button = new Button("X");
     button.setToolTip(new Label("Remove this axis from the chart"));
     button.addActionListener(deleteHandler);
     add(button);
 
     verticalLabel = new VerticalLabel();
+   
+   
     verticalLabel.setTextAlignment(PositionConstants.TOP);
 
     add(verticalLabel);
@@ -31,7 +38,24 @@ public class AxisNameFigure extends RectangleFigure
 
   public void setName(String name)
   {
+    
     verticalLabel.setText(name);
+    // cache font for AxisNameFigure
+    if (boldFont == null)
+    {
+      FontData fontData = verticalLabel.getFont().getFontData()[0];
+      boldFont =
+          new Font(Display.getCurrent(), new FontData(fontData.getName(),
+              fontData.getHeight(), SWT.BOLD));
+    }
+
+    verticalLabel.setFont(boldFont);
+  }
+  
+  @Override
+  public void setFont(Font f)
+  {
+    verticalLabel.setFont(boldFont);
   }
 
 }
