00563be56036d834325f708eca295a67db8688f9
Anuradha Gunasekara <theanuradha@gmail.com>
2016-06-20 22:44:27 +0530

    create support for axis direction

--- info.limpet.stackedcharts.ui/src/info/limpet/stackedcharts/ui/view/ChartBuilder.java
+++ info.limpet.stackedcharts.ui/src/info/limpet/stackedcharts/ui/view/ChartBuilder.java
@@ -1,6 +1,7 @@
 package info.limpet.stackedcharts.ui.view;
 
 import info.limpet.stackedcharts.model.AbstractAnnotation;
+import info.limpet.stackedcharts.model.AxisDirection;
 import info.limpet.stackedcharts.model.AxisType;
 import info.limpet.stackedcharts.model.Chart;
 import info.limpet.stackedcharts.model.ChartSet;
@@ -254,6 +255,10 @@ public class ChartBuilder
         helper = new NumberHelper();
       }
       sharedAxis = helper.createAxis(sharedAxisModel.getName());
+      if(sharedAxisModel.getDirection()==AxisDirection.DESCENDING)
+      {
+        sharedAxis.setInverted(true);
+      }
     }
 
     final CombinedDomainXYPlot plot = new CombinedDomainXYPlot(sharedAxis);
@@ -316,6 +321,10 @@ public class ChartBuilder
         helper = new NumberHelper();
       }
       sharedAxis = helper.createAxis(sharedAxisModel.getName());
+      if(sharedAxisModel.getDirection()==AxisDirection.DESCENDING)
+      {
+        sharedAxis.setInverted(true);
+      }
     }
     sharedAxis.setVisible(false);
     final CombinedDomainXYPlot plot = new CombinedDomainXYPlot(sharedAxis);
@@ -421,6 +430,11 @@ public class ChartBuilder
     final XYDataset collection = axeshelper.createCollection();
 
     final ValueAxis chartAxis = new NumberAxis(dependentAxis.getName());
+    
+    if(dependentAxis.getDirection()==AxisDirection.DESCENDING)
+    {
+      chartAxis.setInverted(true);
+    }
     addDatasetToAxis(axeshelper, dependentAxis.getDatasets(), collection,
         renderer, indexSeries);
 
@@ -430,6 +444,11 @@ public class ChartBuilder
     subplot.setRangeAxis(indexAxis, chartAxis);
     subplot.setRenderer(indexAxis, renderer);
     subplot.mapDatasetToRangeAxis(indexAxis, indexAxis);
+    
+    if(dependentAxis.getDirection()==AxisDirection.DESCENDING)
+    {
+      
+    }
   }
 
   /**
