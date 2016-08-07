fe16fc3ec5e10fde839de901ed6fb9a8387ddfaf
Dinko <dinko.ivanov@gmail.com>
2016-08-07 11:13:51 +0300

    handle undo when adding scatter set #358

--- info.limpet.stackedcharts.ui/src/info/limpet/stackedcharts/ui/editor/commands/AddScatterSetsToChartCommand.java
+++ info.limpet.stackedcharts.ui/src/info/limpet/stackedcharts/ui/editor/commands/AddScatterSetsToChartCommand.java
@@ -1,18 +1,33 @@
 package info.limpet.stackedcharts.ui.editor.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.gef.commands.Command;
+
 import info.limpet.stackedcharts.model.Chart;
 import info.limpet.stackedcharts.model.ChartSet;
 import info.limpet.stackedcharts.model.ScatterSet;
 import info.limpet.stackedcharts.model.SelectiveAnnotation;
 import info.limpet.stackedcharts.model.StackedchartsFactory;
 
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.gef.commands.Command;
-
 public class AddScatterSetsToChartCommand extends Command
 {
   private final ScatterSet[] scatterSets;
   private final Chart parent;
+
+  /**
+   * Contains all newly created annotations during {@link #execute()} that need to be removed during
+   * {@link #undo()}
+   */
+  private List<SelectiveAnnotation> createdAnnotations;
+
+  /**
+   * Contains annotations which have been added to appear in the parent. Again the parent needs to
+   * be removed from those during {@link #undo()}.
+   */
+  private List<SelectiveAnnotation> appearInParent;
 
   public AddScatterSetsToChartCommand(Chart parent, ScatterSet... scatterSets)
   {
@@ -23,17 +38,21 @@ public class AddScatterSetsToChartCommand extends Command
   @Override
   public void execute()
   {
+
+    createdAnnotations = new ArrayList<SelectiveAnnotation>();
+    appearInParent = new ArrayList<SelectiveAnnotation>();
+
     for (ScatterSet ds : scatterSets)
     {
       // ok, we may have to add it to the chartset first
       ChartSet charts = parent.getParent();
-      EList<SelectiveAnnotation> annots =
-          charts.getSharedAxis().getAnnotations();
+      EList<SelectiveAnnotation> annots = charts.getSharedAxis()
+          .getAnnotations();
       SelectiveAnnotation host = null;
       for (SelectiveAnnotation annot : annots)
       {
-        if (annot.getAnnotation().getName() != null
-            && annot.getAnnotation().getName().equals(ds.getName()))
+        if (annot.getAnnotation().getName() != null && annot.getAnnotation()
+            .getName().equals(ds.getName()))
         {
           host = annot;
           break;
@@ -45,6 +64,7 @@ public class AddScatterSetsToChartCommand extends Command
         host = StackedchartsFactory.eINSTANCE.createSelectiveAnnotation();
         host.setAnnotation(ds);
         annots.add(host);
+        createdAnnotations.add(host);
       }
 
       // check we're not already in that chart
@@ -52,6 +72,7 @@ public class AddScatterSetsToChartCommand extends Command
       if (!appearsIn.contains(parent))
       {
         appearsIn.add(parent);
+        appearInParent.add(host);
       }
     }
   }
@@ -59,11 +80,17 @@ public class AddScatterSetsToChartCommand extends Command
   @Override
   public void undo()
   {
-    for (@SuppressWarnings("unused")
-    ScatterSet ds : scatterSets)
+    for (SelectiveAnnotation annotation : appearInParent)
     {
-      // parent.getDatasets().remove(ds);
-      System.err.println("UNDO ADDING SCATTERSET NOT IMPLEMENTED");
+      annotation.getAppearsIn().remove(parent);
+    }
+
+    ChartSet charts = parent.getParent();
+    EList<SelectiveAnnotation> annotations = charts.getSharedAxis()
+        .getAnnotations();
+    for (SelectiveAnnotation annotation : createdAnnotations)
+    {
+      annotations.remove(annotation);
     }
   }
 }
