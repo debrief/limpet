979efd63e7af30a3e1b3858af1a9e23bcda1a47d
Anuradha G <theanuradha@gmail.com>
2017-02-22 20:24:36 +0530

    E4.5 runtime

--- info.limpet.rcp/src/info/limpet/rcp/product/Application.java
+++ info.limpet.rcp/src/info/limpet/rcp/product/Application.java
@@ -431,9 +431,9 @@ public class Application implements IApplication, IExecutableExtension
     // At this point workspace has been detected to be from a version
     // other than the current ide version -- find out if the user wants
     // to use it anyhow.
-    String title = IDEWorkbenchMessages.IDEApplication_versionTitle;
+    String title = IDEWorkbenchMessages.IDEApplication_versionTitle_newerWorkspace;
     String message =
-        NLS.bind(IDEWorkbenchMessages.IDEApplication_versionMessage,
+        NLS.bind(IDEWorkbenchMessages.IDEApplication_versionMessage_newerWorkspace,
             url.getFile());
 
     MessageBox mbox =
