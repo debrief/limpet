940f6f717ebc57e0bcedd0cdd520b8a1b0a7c146
Ian Mayo <ian@planetmayo.com>
2017-06-15 16:27:00 +0100

    Fix test, to allow for new method

--- info.limpet.test/src/info/limpet/data2/TestOperations.java
+++ info.limpet.test/src/info/limpet/data2/TestOperations.java
@@ -1380,7 +1380,7 @@ public class TestOperations
     HashMap<String, List<IOperation>> ops = OperationsLibrary.getOperations();
 
     List<IOperation> create = ops.get(OperationsLibrary.CREATE);
-    assertEquals("Creation size", 6, create.size());
+    assertEquals("Creation size", 7, create.size());
     // Administrator Operations.
 
     List<IOperation> adminOperations =
