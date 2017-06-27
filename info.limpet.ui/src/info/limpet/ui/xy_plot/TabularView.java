28db1f0f2de2357aca51a40442ff3eb7add8133c
Ian Mayo <ian@planetmayo.com>
2017-06-27 15:33:41 +0100

    clean up

--- info.limpet.ui/src/info/limpet/ui/xy_plot/TabularView.java
+++ info.limpet.ui/src/info/limpet/ui/xy_plot/TabularView.java
@@ -47,134 +47,20 @@ import org.eclipse.swt.widgets.Text;
 public class TabularView extends CommonGridView
 {
 
-  /**
-   * The ID of the view as specified by the extension.
-   */
-  public static final String ID = "info.limpet.ui.TabularView";
-
-  private TableViewer table;
-  
-  private DecimalFormat _formatter;
-
-  public TabularView()
-  {
-    super(ID, "Tabular view");
-    _formatter = new DecimalFormat("0.00");
-
-  }
-
-  /**
-   * This is a callback that will allow us to create the viewer and initialize it.
-   */
-  @Override
-  public void createPartControl(Composite parent)
-  {
-
-    // Create the composite to hold the controls
-    Composite composite = new Composite(parent, SWT.NONE);
-    composite.setLayout(new GridLayout(1, false));
-
-    // Create the combo to hold the team names
-    titleLbl = new Text(composite, SWT.NONE);
-    titleLbl.setText(" ");
-
-    // Create the table viewer
-    table =
-        new TableViewer(composite, SWT.H_SCROLL | SWT.V_SCROLL | SWT.NONE);
-
-    final Table tableCtrl = table.getTable();
-    tableCtrl.setHeaderVisible(true);
-    tableCtrl.setLinesVisible(true);
-
-    // sort out the content provider
-    MyContentProvider provider = new MyContentProvider();
-    table.setContentProvider(provider);
-    IBaseLabelProvider labeller = new MyLabelProvider();
-    table.setLabelProvider(labeller);
-
-    tableCtrl.setLayoutData(new GridData(GridData.FILL_BOTH));
-    
-    makeActions();
-    contributeToActionBars();
-    
-    // register as selection listener
-    setupListener();
-  }
-
-  private class MyLabelProvider implements ITableLabelProvider
-  {
-    
-    @Override
-    public void addListener(ILabelProviderListener listener)
-    {
-    }
-
-    @Override
-    public void dispose()
-    {
-    }
-
-    @Override
-    public boolean isLabelProperty(Object element, String property)
-    {
-      return false;
-    }
-
-    @Override
-    public void removeListener(ILabelProviderListener listener)
-    {
-    }
-
-    @Override
-    public Image getColumnImage(Object element, int columnIndex)
-    {
-      return null;
-    }
-
-    @Override
-    public String getColumnText(Object element, int columnIndex)
-    {
-      @SuppressWarnings("unchecked")
-      List<Double> items = (List<Double>) element;
-      final String res;
-
-      Double thisD = items.get(columnIndex);
-
-      if (columnIndex == 0)
-      {
-        res = _formatter.format(thisD) + ":";
-      }
-      else
-      {
-
-        if ((thisD != null) && !thisD.equals(Double.NaN))
-        {
-          res = _formatter.format(thisD);
-        }
-        else
-        {
-
-          res = " ";
-        }
-      }
-      return res;
-    }
-  }
-
   private static class MyContentProvider extends ArrayContentProvider
   {
 
     @Override
-    public Object[] getElements(Object inputElement)
+    public Object[] getElements(final Object inputElement)
     {
       if (inputElement instanceof HContainer)
       {
-        HContainer nd = (HContainer) inputElement;
-        Object[] data = new Object[nd.rowTitles.length];
+        final HContainer nd = (HContainer) inputElement;
+        final Object[] data = new Object[nd.rowTitles.length];
 
         for (int i = 0; i < nd.rowTitles.length; i++)
         {
-          List<Double> thisR = new ArrayList<Double>();
+          final List<Double> thisR = new ArrayList<Double>();
           // start off with the row title
           thisR.add(nd.rowTitles[i]);
 
@@ -192,77 +78,83 @@ public class TabularView extends CommonGridView
     }
   }
 
-  protected void clearChart()
+  private class MyLabelProvider implements ITableLabelProvider
   {
-    // clear the columns
-    Table ctrl = table.getTable();
-    TableColumn[] cols = ctrl.getColumns();
-    for (final TableColumn thisC : cols)
+
+    @Override
+    public void addListener(final ILabelProviderListener listener)
     {
-      thisC.dispose();
     }
-    
-    table.setInput(null);
+
+    @Override
+    public void dispose()
+    {
+    }
+
+    @Override
+    public Image getColumnImage(final Object element, final int columnIndex)
+    {
+      return null;
+    }
+
+    @Override
+    public String getColumnText(final Object element, final int columnIndex)
+    {
+      @SuppressWarnings("unchecked")
+      final List<Double> items = (List<Double>) element;
+      final String res;
+
+      final Double thisD = items.get(columnIndex);
+
+      if (columnIndex == 0)
+      {
+        res = _formatter.format(thisD) + ":";
+      }
+      else
+      {
+        if ((thisD != null) && !thisD.equals(Double.NaN))
+        {
+          res = _formatter.format(thisD);
+        }
+        else
+        {
+          res = " ";
+        }
+      }
+      return res;
+    }
+
+    @Override
+    public boolean isLabelProperty(final Object element, final String property)
+    {
+      return false;
+    }
+
+    @Override
+    public void removeListener(final ILabelProviderListener listener)
+    {
+    }
   }
 
-  protected void show(IStoreItem item)
+  /**
+   * The ID of the view as specified by the extension.
+   */
+  public static final String ID = "info.limpet.ui.TabularView";
+
+  private TableViewer table;
+
+  private final DecimalFormat _formatter;
+
+  public TabularView()
   {
-    NumberDocument thisQ = (NumberDocument) item;
+    super(ID, "Tabular view");
+    _formatter = new DecimalFormat("0.00");
 
-    clearChart();
-
-    final NumberDocument nd = (NumberDocument) item;
-
-    String seriesName = thisQ.getName();
-
-    titleLbl.setText(seriesName + " (" + nd.getUnits().toString() + ")");
-
-
-    // sort out the columns
-    HContainer data = Helper2D.convert((DoubleDataset) nd.getDataset());
-
-    double[] aIndices = data.colTitles;
-
-    // clear the columns
-    Table ctrl = table.getTable();
-    TableColumn[] cols = ctrl.getColumns();
-    for (final TableColumn thisC : cols)
-    {
-      thisC.dispose();
-    }
-
-    // ok. the columns
-    TableColumn header = new TableColumn(ctrl, SWT.RIGHT);
-    header.setText(nd.getIndexUnits().toString());
-    
-    for (double a : aIndices)
-    {
-      TableColumn tc = new TableColumn(ctrl, SWT.RIGHT);
-      tc.setText("" +  _formatter.format(a));
-    }
-
-    // finally get the data displayed
-    table.setInput(data);
-    
-    // lastly, pack the columns (now that we've loaded the data)
-    cols = ctrl.getColumns();
-    for (final TableColumn thisC : cols)
-    {
-      thisC.pack();
-    }    
-    
-    titleLbl.pack();
   }
 
   @Override
-  public void setFocus()
-  {
-    table.getTable().setFocus();
-  }
-
-  @Override
-  protected boolean appliesToMe(List<IStoreItem> res,
-      CollectionComplianceTests tests)
+  protected boolean appliesToMe(final List<IStoreItem> res,
+      final CollectionComplianceTests tests)
   {
     final boolean allNonQuantity = tests.allNonQuantity(res);
     final boolean allCollections = tests.allCollections(res);
@@ -273,9 +165,114 @@ public class TabularView extends CommonGridView
   }
 
   @Override
+  protected void clearChart()
+  {
+    // clear the columns
+    final Table ctrl = table.getTable();
+    final TableColumn[] cols = ctrl.getColumns();
+    for (final TableColumn thisC : cols)
+    {
+      thisC.dispose();
+    }
+
+    table.setInput(null);
+  }
+
+  /**
+   * This is a callback that will allow us to create the viewer and initialize it.
+   */
+  @Override
+  public void createPartControl(final Composite parent)
+  {
+
+    // Create the composite to hold the controls
+    final Composite composite = new Composite(parent, SWT.NONE);
+    composite.setLayout(new GridLayout(1, false));
+
+    // Create the combo to hold the team names
+    titleLbl = new Text(composite, SWT.NONE);
+    titleLbl.setText(" ");
+
+    // Create the table viewer
+    table = new TableViewer(composite, SWT.H_SCROLL | SWT.V_SCROLL | SWT.NONE);
+
+    final Table tableCtrl = table.getTable();
+    tableCtrl.setHeaderVisible(true);
+    tableCtrl.setLinesVisible(true);
+
+    // sort out the content provider
+    final MyContentProvider provider = new MyContentProvider();
+    table.setContentProvider(provider);
+    final IBaseLabelProvider labeller = new MyLabelProvider();
+    table.setLabelProvider(labeller);
+
+    tableCtrl.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+    makeActions();
+    contributeToActionBars();
+
+    // register as selection listener
+    setupListener();
+  }
+
+  @Override
   protected String getTextForClipboard()
   {
     return "Pending";
   }
 
+  @Override
+  public void setFocus()
+  {
+    table.getTable().setFocus();
+  }
+
+  @Override
+  protected void show(final IStoreItem item)
+  {
+    final NumberDocument thisQ = (NumberDocument) item;
+
+    clearChart();
+
+    final NumberDocument nd = (NumberDocument) item;
+
+    final String seriesName = thisQ.getName();
+
+    titleLbl.setText(seriesName + " (" + nd.getUnits().toString() + ")");
+
+    // sort out the columns
+    final HContainer data = Helper2D.convert((DoubleDataset) nd.getDataset());
+
+    final double[] aIndices = data.colTitles;
+
+    // clear the columns
+    final Table ctrl = table.getTable();
+    TableColumn[] cols = ctrl.getColumns();
+    for (final TableColumn thisC : cols)
+    {
+      thisC.dispose();
+    }
+
+    // ok. the columns
+    final TableColumn header = new TableColumn(ctrl, SWT.RIGHT);
+    header.setText(nd.getIndexUnits().toString());
+
+    for (final double a : aIndices)
+    {
+      final TableColumn tc = new TableColumn(ctrl, SWT.RIGHT);
+      tc.setText("" + _formatter.format(a));
+    }
+
+    // finally get the data displayed
+    table.setInput(data);
+
+    // lastly, pack the columns (now that we've loaded the data)
+    cols = ctrl.getColumns();
+    for (final TableColumn thisC : cols)
+    {
+      thisC.pack();
+    }
+
+    titleLbl.pack();
+  }
 }
