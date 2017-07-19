f64db891e1c9fc012d0643bce1823618515eba9b
Ian Mayo <ian@planetmayo.com>
2017-07-19 11:15:41 +0100

    check we produce the correct operation

--- info.limpet.test/src/info/limpet/data2/TestAdmin.java
+++ info.limpet.test/src/info/limpet/data2/TestAdmin.java
@@ -1,20 +1,22 @@
 package info.limpet.data2;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import info.limpet.ICommand;
 import info.limpet.IContext;
-import info.limpet.IStoreGroup;
 import info.limpet.IStoreItem;
 import info.limpet.impl.MockContext;
 import info.limpet.impl.NumberDocument;
 import info.limpet.impl.SampleData;
 import info.limpet.impl.StoreGroup;
 import info.limpet.operations.admin.CreateNewIndexedDatafileOperation;
-import info.limpet.persistence.csv.CsvParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
+
+
 import junit.framework.TestCase;
 
 public class TestAdmin extends TestCase
@@ -50,11 +52,29 @@ public class TestAdmin extends TestCase
     
     selection.clear();    
     selection.add(sampleData.get(SampleData.SPEED_ONE));
-    selection.add(sampleData.get(SampleData.SPEED_LOOKUP));
+    selection.add(sampleData.get(SampleData.SPEED_ONE_RAD));
     
     List<ICommand> ops = oper.actionsFor(selection, store, context);
-    assertEquals("action created", 1, ops.size());
+    assertEquals("action created", 2, ops.size());
+    
+    assertEquals("store empty", 0, store.size());
+    ops.get(0).execute();
+    assertEquals("store populated", 1, store.size());
 
+    NumberDocument output = (NumberDocument) ops.get(0).getOutputs().get(0);
+    
+    // check the units
+    assertEquals("right x axis", SI.METRE.divide(SI.SECOND), output.getIndexUnits());
+    assertEquals("right y axis", NonSI.DECIBEL, output.getUnits());
+
+    // and now the other way around
+    ops.get(1).execute();
+    output = (NumberDocument) ops.get(1).getOutputs().get(0);
+    assertEquals("store populated", 2, store.size());
+    
+    // check the units
+    assertEquals("right x axis", NonSI.DECIBEL, output.getIndexUnits());
+    assertEquals("right y axis", SI.METRE.divide(SI.SECOND), output.getUnits());
   }
 
 }
