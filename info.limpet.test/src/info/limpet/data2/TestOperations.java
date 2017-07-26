be2b09f4ea5c03fccc41a6d740d8f118c42b075a
Ian Mayo <ian@planetmayo.com>
2017-07-26 14:06:42 +0100

    fix the create singleton tests

--- info.limpet.test/src/info/limpet/data2/TestOperations.java
+++ info.limpet.test/src/info/limpet/data2/TestOperations.java
@@ -1354,17 +1354,30 @@ public class TestOperations
         .size());
     ICommand singleGenCommand = singleGeneratorActionFor.iterator().next();
 
+    final String outName = "new_name";
     EasyMock.expect(
-        mockContext.getInput("New variable", "Enter name for variable", ""))
-        .andReturn("new dimensionless").times(1);
+        mockContext.getInput("New variable", "Enter name for variable", "new dimensionless"))
+        .andReturn(outName).times(1);
     EasyMock.expect(
         mockContext.getInput("New variable",
-            "Enter initial value for variable", "")).andReturn("1234.56")
+            "Enter initial value for variable", "100")).andReturn("1234.56")
+        .times(1);
+    EasyMock.expect(
+        mockContext.getInput("New variable",
+            "Enter the range for variable (or cancel to leave un-ranged)", "0:100")).andReturn("5:100")
         .times(1);
     EasyMock.replay(mockContext);
 
     singleGenCommand.execute();
 
+    NumberDocument output = (NumberDocument) singleGenCommand.getOutputs().get(0);
+    assertNotNull(output);
+    assertEquals("correct name", outName, output.getName());
+    assertEquals("correct size", 1, output.size());
+    assertEquals("correct value", 1234.56, output.getValueAt(0), 0.01);
+    assertEquals("correct range", new Range(5d,100d), output.getRange());
+    
+    
   }
 
   @Test
@@ -1736,6 +1749,50 @@ public class TestOperations
     assertTrue(numeric);
     numeric = CsvParser.isNumeric("NAN");
     assertFalse(numeric);
+  }
+
+  @Test
+  public void testCreateSingletonGeneratorInvalid()
+  {
+    StoreGroup store = new SampleData().getData(10);
+    CreateSingletonGenerator generator =
+        new CreateSingletonGenerator("dimensionless", Dimensionless.UNIT);
+    assertNotNull("Create Single Generator is not NULL", generator);
+  
+    List<IStoreItem> selection = new ArrayList<IStoreItem>();
+    StoreGroup storeGroup = new StoreGroup("Track 1");
+    selection.add(storeGroup);
+  
+    IContext mockContext = EasyMock.createMock(MockContext.class);
+  
+    Collection<ICommand> singleGeneratorActionFor =
+        generator.actionsFor(selection, store, mockContext);
+    assertEquals("Create location collection size", 1, singleGeneratorActionFor
+        .size());
+    ICommand singleGenCommand = singleGeneratorActionFor.iterator().next();
+  
+    final String outName = "new_name";
+    EasyMock.expect(
+        mockContext.getInput("New variable", "Enter name for variable", "new dimensionless"))
+        .andReturn(outName).times(1);
+    EasyMock.expect(
+        mockContext.getInput("New variable",
+            "Enter initial value for variable", "100")).andReturn("1234.56")
+        .times(1);
+    EasyMock.expect(
+        mockContext.getInput("New variable",
+            "Enter the range for variable (or cancel to leave un-ranged)", "0:100")).andReturn("")
+        .times(1);
+    EasyMock.replay(mockContext);
+  
+    singleGenCommand.execute();
+  
+    NumberDocument output = (NumberDocument) singleGenCommand.getOutputs().get(0);
+    assertNotNull(output);    
+    assertEquals("correct name", outName, output.getName());
+    assertEquals("correct size", 1, output.size());
+    assertEquals("correct value", 1234.56, output.getValueAt(0), 0.01);
+    assertEquals("correct range", null, output.getRange());
   }
 
 }
